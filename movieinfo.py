63de9c1579bc929d5c480183a38d58261aab2391
Tom Yang <wyh.aaron@gmail.com>
2021-05-14 13:22:28 -0400

    add message when elements are not complete

--- movieinfo.py
+++ movieinfo.py
@@ -29,32 +29,35 @@ async def send_pic(event):
     except:
         await bot.send_message(chat_id, '好像没搜到，换个名字试试')
         return None
-    tmdb_info = requests.get('https://api.themoviedb.org/3/movie/'+str(tmdb_id)+'?api_key='+tmdb_key+'&language=zh-CN').json()
-    trailer_list = requests.get('https://api.themoviedb.org/3/movie/'+str(tmdb_id)+'/videos?api_key='+tmdb_key).json()['results']
-    trailer_url = None
-    for i in trailer_list:
-        if i['site'] == 'YouTube':
-            if i['type'] == 'Trailer':
-                trailer_url = 'https://www.youtube.com/watch?v='+i['key']
-                break
-    if trailer_url is None:
-        trailer = ''
-    else:
-        trailer = ' [预告片]('+trailer_url+')'
-    imdb_id = requests.get('https://api.themoviedb.org/3/movie/'+str(tmdb_id)+'/external_ids?api_key='+tmdb_key).json()['imdb_id']
-    imdb_rating = re.search(r'"ratingValue">\d\.\d', requests.get('https://www.imdb.com/title/'+imdb_id+'/').text).group()[-3:]
-    poster = BytesIO(requests.get('https://www.themoviedb.org/t/p/w600_and_h900_bestv2'+tmdb_info['poster_path'], headers=header).content)
-    countries = dict(countries_for_language('zh_CN'))
-    tmdb_credits = requests.get('https://api.themoviedb.org/3/movie/'+str(tmdb_id)+'/credits?api_key='+tmdb_key).json()
-    for crew in tmdb_credits['crew']:
-            if crew['job'] == 'Director':
-                director = crew['name']
-                break
-    if len(tmdb_info['genres']) >= 2:
-        info = '**'+tmdb_info['title']+' '+tmdb_info['original_title']+' ('+tmdb_info['release_date'][:4]+')**'+trailer+'\n\n'+tmdb_info['overview']+'\n\n导演 '+director+'\n类型 #'+tmdb_info['genres'][0]['name']+' #'+tmdb_info['genres'][1]['name']+'\n国家 #'+countries[tmdb_info['production_countries'][0]['iso_3166_1']]+'\n语言 #'+tmdb_info['spoken_languages'][0]['name']+'\n上映 '+tmdb_info['release_date']+'\n片长 '+str(tmdb_info['runtime'])+'分钟\n#IMDB_'+imdb_rating[0]+' '+imdb_rating
-    else:
-        info = '**'+tmdb_info['title']+' '+tmdb_info['original_title']+' ('+tmdb_info['release_date'][:4]+')**'+trailer+'\n\n'+tmdb_info['overview']+'\n\n导演 '+director+'\n类型 #'+tmdb_info['genres'][0]['name']+'\n国家 #'+countries[tmdb_info['production_countries'][0]['iso_3166_1']]+'\n语言 #'+tmdb_info['spoken_languages'][0]['name']+'\n上映 '+tmdb_info['release_date']+'\n片长 '+str(tmdb_info['runtime'])+'分钟\n#IMDB_'+imdb_rating[0]+' '+imdb_rating
-    await bot.send_file(chat_id, poster, caption=info)
+    try:
+        tmdb_info = requests.get('https://api.themoviedb.org/3/movie/'+str(tmdb_id)+'?api_key='+tmdb_key+'&language=zh-CN').json()
+        trailer_list = requests.get('https://api.themoviedb.org/3/movie/'+str(tmdb_id)+'/videos?api_key='+tmdb_key).json()['results']
+        trailer_url = None
+        for i in trailer_list:
+            if i['site'] == 'YouTube':
+                if i['type'] == 'Trailer':
+                    trailer_url = 'https://www.youtube.com/watch?v='+i['key']
+                    break
+        if trailer_url is None:
+            trailer = ''
+        else:
+            trailer = ' [预告片]('+trailer_url+')'
+        imdb_id = requests.get('https://api.themoviedb.org/3/movie/'+str(tmdb_id)+'/external_ids?api_key='+tmdb_key).json()['imdb_id']
+        imdb_rating = re.search(r'"ratingValue">\d\.\d', requests.get('https://www.imdb.com/title/'+imdb_id+'/').text).group()[-3:]
+        poster = BytesIO(requests.get('https://www.themoviedb.org/t/p/w600_and_h900_bestv2'+tmdb_info['poster_path'], headers=header).content)
+        countries = dict(countries_for_language('zh_CN'))
+        tmdb_credits = requests.get('https://api.themoviedb.org/3/movie/'+str(tmdb_id)+'/credits?api_key='+tmdb_key).json()
+        for crew in tmdb_credits['crew']:
+                if crew['job'] == 'Director':
+                    director = crew['name']
+                    break
+        if len(tmdb_info['genres']) >= 2:
+            info = '**'+tmdb_info['title']+' '+tmdb_info['original_title']+' ('+tmdb_info['release_date'][:4]+')**'+trailer+'\n\n'+tmdb_info['overview']+'\n\n导演 '+director+'\n类型 #'+tmdb_info['genres'][0]['name']+' #'+tmdb_info['genres'][1]['name']+'\n国家 #'+countries[tmdb_info['production_countries'][0]['iso_3166_1']]+'\n语言 #'+tmdb_info['spoken_languages'][0]['name']+'\n上映 '+tmdb_info['release_date']+'\n片长 '+str(tmdb_info['runtime'])+'分钟\n#IMDB_'+imdb_rating[0]+' '+imdb_rating
+        else:
+            info = '**'+tmdb_info['title']+' '+tmdb_info['original_title']+' ('+tmdb_info['release_date'][:4]+')**'+trailer+'\n\n'+tmdb_info['overview']+'\n\n导演 '+director+'\n类型 #'+tmdb_info['genres'][0]['name']+'\n国家 #'+countries[tmdb_info['production_countries'][0]['iso_3166_1']]+'\n语言 #'+tmdb_info['spoken_languages'][0]['name']+'\n上映 '+tmdb_info['release_date']+'\n片长 '+str(tmdb_info['runtime'])+'分钟\n#IMDB_'+imdb_rating[0]+' '+imdb_rating
+        await bot.send_file(chat_id, poster, caption=info)
+    except:
+        await bot.send_message(chat_id, '此片信息不完整，详见：[链接](https://www.themoviedb.org/movie/'+str(tmdb_id)+')')
 
 @bot.on(events.NewMessage(pattern=r'^出题$|^出題$'))
 async def send_question(event):
