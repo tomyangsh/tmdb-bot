2790f148d6b3e53414c3378454d66f8745fdc0ed
Tom Yang <wyh.aaron@gmail.com>
2021-05-14 04:38:25 -0400

    add trailer

--- movieinfo.py
+++ movieinfo.py
@@ -10,12 +10,14 @@ token = os.getenv("TOKEN")
 app_id = int(os.getenv("APP_ID"))
 app_hash = os.getenv("APP_HASH")
 tmdb_key = 'b729fb42b650d53389fb933b99f4b072'
+header = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0'}
 
 tmdb_id = []
 for item in open('movieid'):
     tmdb_id.append(item.strip("\n"))
 
 bot = TelegramClient('bot', app_id, app_hash).start(bot_token=token)
+
 
 @bot.on(events.NewMessage(pattern=r'^/m\s'))
 async def send_pic(event):
@@ -28,9 +30,14 @@ async def send_pic(event):
         await bot.send_message(chat_id, '好像没搜到，换个名字试试')
         return None
     tmdb_info = requests.get('https://api.themoviedb.org/3/movie/'+str(tmdb_id)+'?api_key='+tmdb_key+'&language=zh-CN').json()
+    trailer_list = requests.get('https://api.themoviedb.org/3/movie/'+str(tmdb_id)+'/videos?api_key='+tmdb_key).json()['results']
+    for i in trailer_list:
+        if i['site'] == 'YouTube':
+            if i['type'] == 'Trailer':
+                trailer_url = 'https://www.youtube.com/watch?v='+i['key']
     imdb_id = requests.get('https://api.themoviedb.org/3/movie/'+str(tmdb_id)+'/external_ids?api_key='+tmdb_key).json()['imdb_id']
     imdb_rating = re.search(r'"ratingValue">\d\.\d', requests.get('https://www.imdb.com/title/'+imdb_id+'/').text).group()[-3:]
-    poster = BytesIO(requests.get('https://www.themoviedb.org/t/p/w600_and_h900_bestv2'+tmdb_info['poster_path']).content)
+    poster = BytesIO(requests.get('https://www.themoviedb.org/t/p/w600_and_h900_bestv2'+tmdb_info['poster_path'], headers=header).content)
     countries = dict(countries_for_language('zh_CN'))
     tmdb_credits = requests.get('https://api.themoviedb.org/3/movie/'+str(tmdb_id)+'/credits?api_key='+tmdb_key).json()
     for crew in tmdb_credits['crew']:
@@ -38,9 +45,9 @@ async def send_pic(event):
                 director = crew['name']
                 break
     if len(tmdb_info['genres']) >= 2:
-        info = '**'+tmdb_info['title']+' '+tmdb_info['original_title']+' ('+tmdb_info['release_date'][:4]+')**\n\n'+tmdb_info['overview']+'\n\n导演 '+director+'\n类型 #'+tmdb_info['genres'][0]['name']+' #'+tmdb_info['genres'][1]['name']+'\n国家 #'+countries[tmdb_info['production_countries'][0]['iso_3166_1']]+'\n语言 #'+tmdb_info['spoken_languages'][0]['name']+'\n上映日期 '+tmdb_info['release_date']+'\n片长 '+str(tmdb_info['runtime'])+'分钟\n#IMDB_'+imdb_rating[0]+' '+imdb_rating
+        info = '**'+tmdb_info['title']+' '+tmdb_info['original_title']+' ('+tmdb_info['release_date'][:4]+')** [预告片]('+trailer_url+')\n\n'+tmdb_info['overview']+'\n\n导演 '+director+'\n类型 #'+tmdb_info['genres'][0]['name']+' #'+tmdb_info['genres'][1]['name']+'\n国家 #'+countries[tmdb_info['production_countries'][0]['iso_3166_1']]+'\n语言 #'+tmdb_info['spoken_languages'][0]['name']+'\n上映 '+tmdb_info['release_date']+'\n片长 '+str(tmdb_info['runtime'])+'分钟\n#IMDB_'+imdb_rating[0]+' '+imdb_rating
     else:
-        info = '**'+tmdb_info['title']+' '+tmdb_info['original_title']+' ('+tmdb_info['release_date'][:4]+')**\n\n'+tmdb_info['overview']+'\n\n导演 '+director+'\n类型 #'+tmdb_info['genres'][0]['name']+'\n国家 #'+countries[tmdb_info['production_countries'][0]['iso_3166_1']]+'\n语言 #'+tmdb_info['spoken_languages'][0]['name']+'\n上映日期 '+tmdb_info['release_date']+'\n片长 '+str(tmdb_info['runtime'])+'分钟\n#IMDB_'+imdb_rating[0]+' '+imdb_rating
+        info = '**'+tmdb_info['title']+' '+tmdb_info['original_title']+' ('+tmdb_info['release_date'][:4]+')** [预告片]('+trailer_url+')\n\n'+tmdb_info['overview']+'\n\n导演 '+director+'\n类型 #'+tmdb_info['genres'][0]['name']+'\n国家 #'+countries[tmdb_info['production_countries'][0]['iso_3166_1']]+'\n语言 #'+tmdb_info['spoken_languages'][0]['name']+'\n上映 '+tmdb_info['release_date']+'\n片长 '+str(tmdb_info['runtime'])+'分钟\n#IMDB_'+imdb_rating[0]+' '+imdb_rating
     await bot.send_file(chat_id, poster, caption=info)
 
 @bot.on(events.NewMessage(pattern=r'^出题$'))
@@ -67,7 +74,7 @@ async def send_question(event):
         image_url = 'https://www.themoviedb.org/t/p/original'+random.choice(image_list)['file_path']
     except:
         image_url = 'https://www.themoviedb.org/t/p/original'+tmdb_info['poster_path']
-    image = BytesIO(requests.get(image_url).content)
+    image = BytesIO(requests.get(image_url, headers=header).content)
     try:
         async with bot.conversation(event.message.chat_id, exclusive=False, timeout=60) as conv:
             question = await conv.send_file(image, caption=caption1)
