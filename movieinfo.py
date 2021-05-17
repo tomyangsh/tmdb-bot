c21459e8e55be6a58082e9d47571412a513905f3
Tom Yang <wyh.aaron@gmail.com>
2021-05-17 07:55:29 -0400

    use trakt rating for tv

--- movieinfo.py
+++ movieinfo.py
@@ -1,4 +1,4 @@
-import requests, re, json, os, urllib.request, shutil, random
+import requests, re, json, os, urllib.request, shutil, random, traceback
 
 from io import BytesIO
 
@@ -20,6 +20,13 @@ langcode = {}
 for line in open('langcode'):
     key, value = line.split(' ')
     langcode[key] = value.strip('\n')
+
+status_dic = {
+        'Returning Series': '在播',
+        'Ended': '完结',
+        'Canceled': '被砍',
+        'In Production': '拍摄中'
+        }
 
 def get_translation(text):
     result = requests.get('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=zh-cn&&dt=t&q='+requests.utils.quote(text)).json()[0][0][0]
@@ -74,13 +81,13 @@ async def movie_info(event):
             info = '**'+tmdb_info['title']+' '+tmdb_info['original_title']+' ('+tmdb_info['release_date'][:4]+')**'+trailer+'\n\n'+tmdb_info['overview']+'\n\n导演 '+director+'\n类型'+genres+'\n国家 '+countries[tmdb_info['production_countries'][0]['iso_3166_1']]+'\n语言 '+language+'\n上映 '+tmdb_info['release_date']+'\n片长 '+str(tmdb_info['runtime'])+'分钟\n演员 '+actors+'\n#IMDB_'+imdb_rating[0]+' '+imdb_rating
         await bot.send_file(chat_id, poster, caption=info)
     except Exception as e:
-        print(e)
+        traceback.print_exc()
         await bot.send_message(chat_id, '此片信息不完整，详见：[链接](https://www.themoviedb.org/movie/'+str(tmdb_id)+')')
 
 @bot.on(events.NewMessage(pattern=r'^/t\s'))
 async def tv_info(event):
     chat_id = event.message.chat_id
-    msg = re.sub(r'/m\s*', '', event.message.text)
+    msg = re.sub(r'/t\s*', '', event.message.text)
     if re.search(r'\s\d*$', msg):
         search_query = re.match(r'.*\s', msg).group()[:-1]+'&year='+re.search(r'\s\d*$', msg).group()
     else:
@@ -104,14 +111,14 @@ async def tv_info(event):
             trailer = ''
         else:
             trailer = ' [预告片]('+trailer_url+')'
-        imdb_id = requests.get('https://api.themoviedb.org/3/movie/'+str(tmdb_id)+'/external_ids?api_key='+tmdb_key).json()['imdb_id']
-        imdb_rating = re.search(r'"ratingValue">\d\.\d', requests.get('https://www.imdb.com/title/'+imdb_id+'/').text).group()[-3:]
+        imdb_id = requests.get('https://api.themoviedb.org/3/tv/'+str(tmdb_id)+'/external_ids?api_key='+tmdb_key).json()['imdb_id']
+        trakt_rating = str(requests.get('https://api.trakt.tv/shows/'+imdb_id+'/ratings', headers={'trakt-api-key': '4fb92befa9b5cf6c00c1d3fecbd96f8992c388b4539f5ed34431372bbee1eca8'}).json()['rating'])[:3]
         poster = BytesIO(requests.get('https://www.themoviedb.org/t/p/w600_and_h900_bestv2'+tmdb_info['poster_path'], headers=header).content)
         countries = dict(countries_for_language('zh_CN'))
-        tmdb_credits = requests.get('https://api.themoviedb.org/3/movie/'+str(tmdb_id)+'/credits?api_key='+tmdb_key).json()
-        for crew in tmdb_credits['crew']:
-                if crew['job'] == 'Creator':
-                    creator = ' '+re.sub('（.*）', '', get_translation(crew['name']))
+        tmdb_credits = requests.get('https://api.themoviedb.org/3/tv/'+str(tmdb_id)+'/credits?api_key='+tmdb_key).json()
+        creator = ''
+        for person in tmdb_info['created_by']:
+                    creator = creator+' '+re.sub('（.*）', '', get_translation(person['name']))
         actors = re.sub('（.*）', '', get_translation(tmdb_credits['cast'][0]['name']))+'\n'
         for item in tmdb_credits['cast'][1:5]:
             actor = re.sub('（.*）', '', get_translation(item['name']))
@@ -119,13 +126,14 @@ async def tv_info(event):
         genres = ''
         for genre in tmdb_info['genres'][:2]:
             genres = genres+' #'+genre['name']
-            seasons = ''
-            for season in tmdb_info['seasons']:
-                seasons = seasons+season['name']+' - 共'+season['episode_count']+'集'
-            info = '**'+tmdb_info['title']+' '+tmdb_info['original_title']+' ('+tmdb_info['release_date'][:4]+')**'+trailer+'\n\n'+tmdb_info['overview']+'\n\n创作'+creator+'\n类型'+genres+'\n国家 '+countries[tmdb_info['production_countries'][0]['iso_3166_1']]+'\n网络 '+tmdb_info['networks']['name']+'\n首播 '+tmdb_info['first_air_date']+'\n集长 '+str(tmdb_info['episode_run_time'])+'分钟\n演员 '+actors+'\n分季概况：\n'+seasons+'\n#IMDB_'+imdb_rating[0]+' '+imdb_rating
+        seasons = ''
+        for season in tmdb_info['seasons']:
+            seasons = seasons+season['name']+' - 共'+str(season['episode_count'])+'集\n'
+        status = status_dic[tmdb_info['status']]
+        info = '**'+tmdb_info['name']+' '+tmdb_info['original_name']+' ('+tmdb_info['first_air_date'][:4]+')**'+trailer+'\n\n'+tmdb_info['overview']+'\n\n创作'+creator+'\n类型'+genres+'\n国家 '+countries[tmdb_info['production_countries'][0]['iso_3166_1']]+'\n网络 #'+tmdb_info['networks'][0]['name']+'\n状况 '+status+'\n首播 '+tmdb_info['first_air_date']+'\n集长 '+str(tmdb_info['episode_run_time'][0])+'分钟\n演员 '+actors+'\n分季概况：\n'+seasons+'\n#Trakt_'+trakt_rating[0]+' '+trakt_rating
         await bot.send_file(chat_id, poster, caption=info)
     except Exception as e:
-        print(e)
+        traceback.print_exc()
         await bot.send_message(chat_id, '此剧信息不完整，详见：[链接](https://www.themoviedb.org/tv/'+str(tmdb_id)+')')
 
 @bot.on(events.NewMessage(pattern=r'^出题$|^出題$'))
